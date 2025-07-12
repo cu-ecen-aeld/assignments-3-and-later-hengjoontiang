296d4f2c197284646f258539f37180880b0d3281
Heng Joon Tiang <hengjoontiang@yahoo.com>
2025-07-12 18:55:20 +0800

    Update finder-test.sh -- more needed changes

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -56,10 +56,11 @@ fi
 for i in $( seq 1 $NUMFILES)
 do
 	#./writer.sh "$WRITEDIR/${username}$i.txt" "$WRITESTR"
-	/bin/writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+        cp "$WRITEDIR/${username}$i.txt"  /tmp/assignment4-result.txt
 done
 
-OUTPUTSTRING=$(/bin/finder.sh "$WRITEDIR" "$WRITESTR")
+OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
 
 # remove temporary directories
 rm -rf /tmp/aeld-data
